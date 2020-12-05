63ccbd3464baf2a4c5af230041eeebb278afe320
mutoni vivine <mutoniv5@gmail.com>
2020-12-05 19:39:13 +0200

    Removed process result in request.py

--- app/requests.py
+++ app/requests.py
@@ -18,30 +18,3 @@ def get_quotes():
     get_quote_response = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
     print(get_quote_response)
     return get_quote_response
-
-# def process_results(movie_list):
-#     '''
-#     Function that processes the movie result and transform them to a list of objects
-
-#     Args:
-#         movie_list: A list of dictionaries that contain movie details
-
-#     Returns:
-#         movie_results: A list of movie objects
-#     '''
-
-#     quote_results = []
-
-#     for quote_item in movie_list:
-#         id = movie_item.get('id')
-#         title = movie_item.get('original_title')
-#         overview = movie_item.get('overview')
-#         poster = movie_item.get('poster_path')
-#         vote_average = movie_item.get('vote_average')
-#         vote_count = movie_item.get('vote_count')
-
-#         if poster:
-#             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-#             movie_results.append(movie_object)
-            
-#     return movie_results
